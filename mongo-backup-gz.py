3e9f29a14ddaf43c21506f00638cff77ae0a9164
Denis Chernyakov <Denis Chernyakov>
2016-12-12 13:54:42 +1100

    test
    test

--- mongo-backup-gz.py
+++ mongo-backup-gz.py
@@ -16,7 +16,7 @@ import zc.lockfile
 from shutil import copyfile, rmtree, copytree, move
 from pymongo import MongoClient
 
-#exclude_db = ('local') 
+exclude_db = ('local') 
 work_dir = "/backup/mongodbbackup/work/"
 cleanup_dir = "/backup//mongodbbackup/storage/daily"
 #mongodb_conf = "/etc/mongod.conf"
