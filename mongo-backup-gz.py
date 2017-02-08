2108252216f08f7ce9bcc9b24ee7121afef773b2
Denis Chernyakov <Denis Chernyakov>
2017-02-09 09:37:22 +1100

    1
    1

--- mongo-backup-gz.py
+++ mongo-backup-gz.py
@@ -62,7 +62,7 @@ def get_last_backup(folder):
 	    a.append(dirs)               
 	    a.sort()
 	    if len(a) > 0:
-		last_backup = a[len(a)-1]
+		last_backup = a[-1]
 		path = os.path.join(folder,last_backup)
 		logging.info("Last backup. %s, path %s" % (last_backup, path))
 		return path
